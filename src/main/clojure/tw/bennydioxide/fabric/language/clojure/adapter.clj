aeb64be55f946a10d0cc3b3c8a1cc89387e415e9
BennyDioxide <bennystyang@gmail.com>
2024-04-27 20:21:35 +0800

    Initial commit
    Setup environment and main file

---
+++ src/main/clojure/tw/bennydioxide/fabric/language/clojure/adapter.clj
@@ -0,0 +1,9 @@
+(ns tw.bennydioxide.fabric.language.clojure.adapter
+  (:gen-class
+    :name tw.bennydioxide.fabric.language.clojure.adapter.ClojureAdapter
+    :implements [net.fabricmc.loader.api.LanguageAdapter]
+    :main false
+    :prefix "-"))
+
+(defn -create [mod value type]
+  ())
---
+++ src/test/clojure/tw/bennydioxide/fabric/language/clojure/test/clojure_adapter_test.clj
@@ -0,0 +1,6 @@
+(ns tw.bennydioxide.fabric.language.clojure.test.clojure-adapter-test
+  (:require [clojure.test :refer :all])
+  (:import (tw.bennydioxide.fabric.language.clojure ClojureAdapter)))
+
+(deftest top-level-entrypoint
+  ())

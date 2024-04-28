aac524ff275e0ffdbe9ca0884e3dbcafb6b0a9d0
BennyDioxide <bennystyang@gmail.com>
2024-04-28 23:52:15 +0800

    feat: Remove stub Clojure adapter temporary
    Fabric Loader can't find Clojure classes in early loading state, so remove

--- src/main/clojure/tw/bennydioxide/fabric/language/clojure/adapter.clj
+++ src/main/clojure/tw/bennydioxide/fabric/language/clojure/adapter.clj
@@ -1,9 +1,9 @@
 (ns tw.bennydioxide.fabric.language.clojure.adapter
+  (:import (net.fabricmc.loader.api LanguageAdapter))
   (:gen-class
-    :name tw.bennydioxide.fabric.language.clojure.adapter.ClojureAdapter
+    :name tw.bennydioxide.fabric.language.clojure.ClojureAdapter
     :implements [net.fabricmc.loader.api.LanguageAdapter]
-    :main false
     :prefix "-"))
 
 (defn -create [mod value type]
-  ())
+  (.create (LanguageAdapter/getDefault) mod value type))

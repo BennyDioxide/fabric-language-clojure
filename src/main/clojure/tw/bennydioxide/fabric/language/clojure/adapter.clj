9eb8561e2b01d63eebb56983c1e40355a23da8de
BennyDioxide <bennystyang@gmail.com>
2024-05-02 00:43:52 +0800

    feat: Add function adapter

--- src/main/clojure/tw/bennydioxide/fabric/language/clojure/adapter.clj
+++ src/main/clojure/tw/bennydioxide/fabric/language/clojure/adapter.clj
@@ -1,9 +1,18 @@
 (ns tw.bennydioxide.fabric.language.clojure.adapter
   (:import (net.fabricmc.loader.api LanguageAdapter))
+  (:require [org.ajoberstar.cljj.function :as cljj.fn])
+  (:import (net.fabricmc.loader.api LanguageAdapterException)
   (:gen-class
     :name tw.bennydioxide.fabric.language.clojure.ClojureAdapter
-    :implements [net.fabricmc.loader.api.LanguageAdapter]
-    :prefix "-"))
+    :implements [net.fabricmc.loader.api.LanguageAdapter]))
 
 (defn -create [_this mod value type]
   (.create (LanguageAdapter/getDefault) mod value type))
+(defn -create [_ _ s ^Class t]
+  (let [method-split (clojure.string/split s #"/")]
+    (if (>= (count method-split) 3)
+      (throw (LanguageAdapterException. (str "Invalid handle format: " s))))
+    (try
+      (case (count method-split)
+        2 (cljj.fn/sam* t (find-var (symbol s))))
+      (catch Exception e (throw (LanguageAdapterException. ^Exception e)))))) ; Clojure can't distinguish two constructors for some reason
